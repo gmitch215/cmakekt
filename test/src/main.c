65bc9d657c803497ada8b31b8fc592c36e136ad8
Gregory Mitchell <me@gmitch215.xyz>
2025-03-11 15:20:44 +0000

    Add `lib2.h` Header

--- test/src/main.c
+++ test/src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <lib.h>
+#include <lib2.h>
+
+// lib1
 
 void myLibraryFunction() {
     printf("Hello from myLibraryFunction!\n");
@@ -16,11 +19,51 @@ char* getString() {
     return "Hello from getString!";
 }
 
+// lib2
+MyStruct* lib2_create() {
+    MyStruct* s = (MyStruct*) malloc(sizeof(MyStruct));
+    s->a = 1;
+    s->b = 'a';
+    s->c = 1234567890;
+    return s;
+}
+
+void lib2_func(MyStruct* s) {
+    printf("MyStruct: %d %c %ld\n", s->a, s->b, s->c);
+}
+
+int** lib2_create_array(int n, int m) {
+    int** arr = (int**) malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        arr[i] = (int*) malloc(m * sizeof(int));
+        for (int j = 0; j < m; j++) {
+            arr[i][j] = i * m + j;
+        }
+    }
+    return arr;
+}
+
+void lib2_free_array(int** arr, int n) {
+    for (int i = 0; i < n; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+}
+
 int main() {
+    // lib1
     myLibraryFunction();
     printf("myLibraryVariable: %d\n", myLibraryVariable);
     printf("getLong: %ld\n", getLong());
     printf("getString: %s\n", getString());
+
+    // lib2
+    MyStruct* s = lib2_create();
+    lib2_func(s);
+    free(s);
+
+    int** arr = lib2_create_array(3, 4);
+    lib2_free_array(arr, 3);
     
     return 0;
 }
